8d2feaedb0a78af64fe9f53f612ff29f59aa4794
Nathaniel Cook <nvcook42@gmail.com>
2018-12-06 07:13:35 -0800

    numeric sorting

--- sort.py
+++ sort.py
@@ -1,29 +1,44 @@
 
 elementSort = {
-        'AIScripts': 'Name',
-        'Sound': 'Name',
-        'EffectsResources': 'Name',
-        'GlobalGameResources': 'Name',
-        'TimelineStatistics': 'Name',
+        'AIScripts': {'tag':'Name','prefix':'Script'},
+        'Sounds': {'tag':'Name','prefix':'Sound'},
+        'EffectsResources': {'tag':'Name','prefix':''},
+        'GlobalGameResources': {'tag':'Name','prefix':''},
+        'TimelineStatistics': {'tag':'Name','prefix':''},
+        'ActionStepModifiers': {'tag':'Name','prefix':''},
+        'ObjectClasses': {'tag':'Name','prefix':''},
         #'UnitLists': '', # Ignore Unit lists for now
-        'ActionStepModifiers': 'Name',
-        'ObjectClasses': 'Name',
 }
 
-def key(e, sortKey):
-    node = e.find(sortKey) 
-    if node is None:
-        return ''
-    v = node.text
-    if v is None:
-        return ''
-    return v
+def strip_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def key(e, tag, prefix):
+    if len(prefix) > 0:
+        node = e.find(tag) 
+        if node is None:
+            return 0
+        v = node.text
+        if v is None:
+            return 0
+        v = strip_prefix(v, prefix)
+        return int(v)
+    else:
+        node = e.find(tag) 
+        if node is None:
+            return ''
+        v = node.text
+        if v is None:
+            return ''
+        return v
 
 def sortTree(tree):
-    for element, sortKey in elementSort.items():
+    for element, info in elementSort.items():
         elements = tree.find(element)
         if elements is not None:
-            elements[:] = sorted(elements, key=lambda e: key(e, sortKey))
+            elements[:] = sorted(elements, key=lambda e: key(e, info['tag'], info['prefix']))
 
 
 
